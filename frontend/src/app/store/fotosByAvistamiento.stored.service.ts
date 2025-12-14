943a0d0a2452f483a0d3922b31b7d0943a69c097
Ana Cossio <ana.cossio@evoltis.com>
2025-12-14 17:22:20 -0300

    feat: hooks/stored de los llamados a las apis

--- frontend/src/app/store/fotosByAvistamiento.stored.service.ts
+++ frontend/src/app/store/fotosByAvistamiento.stored.service.ts
@@ -13,10 +13,10 @@ import { FotoService } from '../services/foto.service';
 import { FotoLinkDTO } from '../interfaces/fotoLinkDTO';
 
 @Injectable()
-export class FotoByUserStoreService {
+export class FotosByAvistamientoStoreService {
   private readonly toastr = inject(ToastrService);
   readonly servicioFoto =inject(FotoService)
-  public fotoUsuario =signal<FotoLinkDTO | null>(null)
+  public fotos =signal<FotoLinkDTO[]>([])
 
   private _status  = signal<ApiStatus>(ApiStatus.INIT);
 
@@ -24,22 +24,21 @@ export class FotoByUserStoreService {
   readonly isSuccess = computed(() => this._status() === ApiStatus.SUCCESS);
   readonly isError   = computed(() => this._status() === ApiStatus.ERROR);
 
-  readonly servicePerfil =inject(UsuarioService)
 
-  public _getFoto(idUsuario:number){
+  public _getFoto(publicacionId:number){
     this._status.set(ApiStatus.LOADING);
 
-    this.servicioFoto.getFotoUsuario(idUsuario)
+    this.servicioFoto.getFotosByPublicacion(publicacionId)
       .subscribe({
       next: (resp) => {
         this._status.set(ApiStatus.SUCCESS);
-        this.fotoUsuario.set(resp)
+        this.fotos.set(resp)
 
       },
       error: (err) => {
         console.error('Login error:', err);
         this._status.set(ApiStatus.ERROR);
-        this.toastr.error('Error al recuperar el perfil', 'Error en el servidor');
+        this.toastr.error('Error al recuperar las fotos del avistamiento', 'Error en el servidor');
       }
     });
   }
