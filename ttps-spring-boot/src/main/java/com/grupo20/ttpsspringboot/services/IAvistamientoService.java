e4fc782e03d0c9572034b39ff9e988e1a8b71eaa
Leandro Svetlich <lsvetlich@fu.do>
2025-12-15 01:37:22 -0300

    fix: faltaba metodo en interfaz

--- ttps-spring-boot/src/main/java/com/grupo20/ttpsspringboot/services/IAvistamientoService.java
+++ ttps-spring-boot/src/main/java/com/grupo20/ttpsspringboot/services/IAvistamientoService.java
@@ -3,7 +3,9 @@ package com.grupo20.ttpsspringboot.services;
 import com.grupo20.ttpsspringboot.domain.models.Avistamiento;
 import com.grupo20.ttpsspringboot.domain.models.Usuario;
 import com.grupo20.ttpsspringboot.dtos.AvistamientoCreateDTO;
+import com.grupo20.ttpsspringboot.dtos.AvistamientoDTO;
 import com.grupo20.ttpsspringboot.dtos.AvistamientoFilterDTO;
+import com.grupo20.ttpsspringboot.dtos.AvistamientoUpdateDTO;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -15,4 +17,6 @@ public interface IAvistamientoService {
     List<Avistamiento> getFiltered(AvistamientoFilterDTO dto);
 
     Avistamiento get(Long id);
+
+    AvistamientoDTO update(Long id, AvistamientoUpdateDTO dto, List<MultipartFile> files);
 }
